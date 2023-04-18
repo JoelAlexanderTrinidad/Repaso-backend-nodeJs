17da1a639be9f578728b73ef93e0e25b33ed9ade
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-04-18 00:58:14 -0300

    servirdor con express

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const puerto = 8080;
+
+app.get('/', (req, res) => res.send('Hola'))
+
+app.listen(puerto, console.log(`Servidor corriendo en http://localhost:${puerto}`));
