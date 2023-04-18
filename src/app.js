558828d68f804dc4573cf9a1c3987d933d0d5e80
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-04-18 01:06:36 -0300

    agregando peticion

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const puerto = 8080;
 
-app.get('/', (req, res) => res.send('Hola'))
+app.get('/', (req, res) => res.send('Soy el index'))
+
+app.get('/productos', (req, res) => {
+    console.log(req.url)
+    return res.send('Soy productos')
+})
 
 app.listen(puerto, console.log(`Servidor corriendo en http://localhost:${puerto}`));
