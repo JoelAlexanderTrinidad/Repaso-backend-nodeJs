ff2e1691faba235dfcbf7ef63ef628b5e5f4ec4b
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-04-18 01:01:44 -0300

    recursos estaticos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const puerto = 8080;
 
-app.get('/', (req, res) => res.send('Hola'))
+// app.get('/', (req, res) => res.send('Hola'))
+
 
 app.listen(puerto, console.log(`Servidor corriendo en http://localhost:${puerto}`));
