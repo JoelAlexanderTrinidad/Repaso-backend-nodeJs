8ebfee236ce17b65603d8ce3559ebe60bf9d52f2
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-04-29 15:19:26 -0300

    crud in progress

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const router = express.Router();
 const puerto = 8080;
 const indexRouter = require('./routes/indexRoutes');
 const productsRouter = require('./routes/productRoutes');
+const logger = require('morgan')
 
 app.set('views', path.join(__dirname, '../public/views'));
 app.set('view engine', 'ejs');
 
-
+app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
